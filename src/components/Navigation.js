9a33cc550b01c8a0127f76fbb1afef95e4e302ca
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-06-18 16:52:12 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ const Nav = () =>{
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-          <label class="navbar-brand ">E-commerce</label>
+          <label class="navbar-brand ">Coumbita</label>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
